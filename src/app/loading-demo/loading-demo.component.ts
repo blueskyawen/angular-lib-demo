93b0c007cb0ad8ae9a97c8fcbd2cba26548fa37e
liuxuwen10140721 <liu.xuwen@zte.com.cn>
2019-05-15 18:11:00 +0800

    add doc

--- src/app/loading-demo/loading-demo.component.ts
+++ src/app/loading-demo/loading-demo.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ 加载中--LoadingDemoComponent
+ */
 @Component({
   selector: 'app-loading-demo',
   templateUrl: './loading-demo.component.html',
   styleUrls: ['./loading-demo.component.less']
 })
 export class LoadingDemoComponent implements OnInit {
-
+  /**
+   * @ignore
+   */
   constructor() { }
 
   ngOnInit() {
+  }
+
+  /**
+   * @example
+   * This is a good example
+   * initDemo(normal)
+   *
+   * @param {string} type  The type to process see {@link http://www.google.com}
+   * @returns The processed target void
+   */
+  initDemo(type: string) : void {
+
   }
 
 }
